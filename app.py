92396eddc94c350bca03255e2360dcd30bbe8b7d
nicolscage <cervantesnicolas35@gmail.com>
2025-03-31 10:04:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hola_mundo():
     return "¡Hola Ivan, esta cosa ya funciona, esto lo escribí en Spyder a las 5:50, ya me quiero ir. Llevo casi 30 minutos peleando porque no sé como subir desde mi compu local (spyder) a GitHub!"  # Esto es lo que verán
 
 if __name__ == '__main__':
-    app.run()  # Inicia el servidor
+    app.run()  # Inicia el servido
