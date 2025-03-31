17e2c74addb113a0fa42e982d971784b20bde067
nicolscage <cervantesnicolas35@gmail.com>
2025-03-31 10:03:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)  # Crea una aplicación web
 
 @app.route('/')  # Cuando alguien visite la página principal
 def hola_mundo():
-    return "¡funciona pedazo de mrd!"  # Esto es lo que verán
+    return "¡Hola Ivan, esta cosa ya funciona, esto lo escribí en Spyder a las 5:50, ya me quiero ir. Llevo casi 30 minutos peleando porque no sé como subir desde mi compu local (spyder) a GitHub!"  # Esto es lo que verán
 
 if __name__ == '__main__':
     app.run()  # Inicia el servidor
